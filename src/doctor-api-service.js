baffd64a05779791cf0fc4561bb2449895992003
Student <example@example.com>
2019-11-30 15:41:19 -0800

    upkeep commit

--- src/doctor-api-service.js
+++ src/doctor-api-service.js
@@ -1,7 +1,7 @@
 export class DoctorAPIService {
   async getDoctorByName(doctorName) {
     try {
-      let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?query=${doctorName}&location=or-portland&user_key=0d5411396d89c97aac569d8df021a9fc`);
+      let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?query=${doctorName}&location=or-portland&user_key= 0d5411396d89c97aac569d8df021a9fc`);
       let jsonifiedResponse = await response.json();
       return jsonifiedResponse;
     } catch(error) {
