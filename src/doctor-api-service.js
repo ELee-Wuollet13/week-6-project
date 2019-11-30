6b46ef6be4a8ebb4a3f1051d69591783ed0c150d
Student <example@example.com>
2019-11-30 13:46:17 -0800

    changed key format and API call

--- src/doctor-api-service.js
+++ src/doctor-api-service.js
@@ -1,7 +1,7 @@
 export class DoctorAPIService {
   async getDoctorByCategory(category) {
     try {
-      let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=0d5411396d89c97aac569d8df021a9fc`);
+      let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&${process.env.API_KEY}`);
       return response;
     } catch(error) {
       console.error(`I'm sorry, there was a problem: ${error.message}`);
