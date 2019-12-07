829a42a27d92c2eb6f2656c750197b5f937e059f
Student <example@example.com>
2019-12-07 13:20:59 -0800

    updated webpack config to include Dotenv plugin and dotenv installation in console

--- src/doctor-api-service.js
+++ src/doctor-api-service.js
@@ -1,7 +1,8 @@
+
 export class DoctorAPIService {
   async getDoctorByName(doctorName) {
     try {
-      let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?name=${doctorName}&query=${doctorName}&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${process.env.API_KEY}`);
+      let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?name=${doctorName}&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${process.env.API_KEY}`);
       console.log(process.env.API_KEY);
       let jsonifiedResponse = await response.json();
       return jsonifiedResponse;
